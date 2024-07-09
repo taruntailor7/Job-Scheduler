f6aa684511f795f909558793ca95756d2e29f8fd
Tarun Tailor <taruntailor7@gmail.com>
2024-07-10 01:36:42 +0530

    made changes in scheduling service

--- backend/src/services/scheduleService.js
+++ backend/src/services/scheduleService.js
@@ -6,7 +6,6 @@ class ScheduleService {
     this.jobs = [];
     this.currentJob = null;
     this.wss = null;
-    this.isProcessing = false;
   }
 
   setWebSocketServer(wss) {
@@ -25,7 +24,6 @@ class ScheduleService {
   }
 
   async addJob(job) {
-    await job.save();  // Save the job to the database first
     this.jobs.push(job);
     this.jobs.sort((a, b) => a.duration - b.duration);
     this.broadcastJobUpdate(job);
@@ -34,41 +32,29 @@ class ScheduleService {
       this.stopCurrentJob();
     }
     
-    if (!this.isProcessing) {
-      this.startProcessing();
+    if (!this.currentJob) {
+      this.processNextJob();
     }
   }
 
-  async startProcessing() {
-    this.isProcessing = true;
-    await this.processNextJob();
-  }
-
   async processNextJob() {
-    if (this.jobs.length > 0) {
+    if (!this.currentJob && this.jobs.length > 0) {
       this.currentJob = this.jobs.shift();
-      this.currentJob.status = 'queued';
-      await this.currentJob.save();
-      this.broadcastJobUpdate(this.currentJob);
       
       // Introduce a delay before starting the job
       this.currentJob.startTimer = setTimeout(async () => {
         this.currentJob.status = 'running';
-        this.currentJob.startTime = new Date();
         await this.currentJob.save();
         this.broadcastJobUpdate(this.currentJob);
 
         this.currentJob.completionTimer = setTimeout(async () => {
           this.currentJob.status = 'completed';
-          this.currentJob.completionTime = new Date();
           await this.currentJob.save();
           this.broadcastJobUpdate(this.currentJob);
           this.currentJob = null;
-          await this.processNextJob();
+          this.processNextJob();
         }, this.currentJob.duration * 1000);
       }, 5000); // 5-second delay before starting the job
-    } else {
-      this.isProcessing = false;
     }
   }
 
